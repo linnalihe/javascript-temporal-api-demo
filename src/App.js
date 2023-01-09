c17d642b0f2e1db63c3f205e7d4212237a7c3b8a
Linna Li <linnali@Linnas-Air.lan>
2023-01-10 07:04:55 +0800

    temporal api

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { Temporal } from '@js-temporal/polyfill';
+
+
+const timeRightNow = Temporal.Now.plainDateTimeISO()
+const timeRightNowWithZone = Temporal.Now.zonedDateTimeISO()
+console.log(timeRightNow)
+console.log(timeRightNowWithZone)
+console.log(new Date())
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <p>Time right now: {timeRightNowWithZone.zonedDateTimeISO}</p>
+        <p>Year right now: {timeRightNowWithZone.year}</p>
+        <p>Month right now: {timeRightNowWithZone.month}</p>
+        <p>Day of month right now: {timeRightNowWithZone.day}</p>
+        <p>Hour right now: {timeRightNowWithZone.hour}</p>
       </header>
     </div>
   );
